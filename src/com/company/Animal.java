b4af2776372710d0704449c4b0fa8689fe1a136e
Covit19 <65606050+Covit19@users.noreply.github.com>
2020-05-19 16:15:59 +0200

    initial commit of the files created by IntelliJ

---
+++ src/com/company/Animal.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Animal {
+    public Animal(){
+    System.out.println("Hello!");
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{}";
+    }
+}
